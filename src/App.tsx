1c313febd626c5f8d43541d2c774578d482d9354
Karel <nagelkarel@gmail.com>
2023-08-31 21:46:46 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-   <form action=""></form>
-  );
+  return <form action="bg-blue-500">terer adsfasd fas fdas f</form>;
 }
 
 export default App;
