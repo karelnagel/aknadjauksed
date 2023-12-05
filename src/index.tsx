1de14b30d0e18e6b674f342a66ebf2130045d2c4
Karel <nagelkarel@gmail.com>
2023-12-05 09:59:40 +0700

    Revert "loading after dom loaded"
    This reverts commit 71b3414450de6c442fb290d09077b14dad822453.

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { type Variant } from "./store";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const element = document.getElementById("root") as HTMLElement;
-  const variant: Variant = element?.getAttribute("data-variant") as Variant;
-  const root = ReactDOM.createRoot(element);
-  root.render(
-    <React.StrictMode>
-      {!variant && <div>Data variant missing</div>}
-      {variant && <App variant={variant} />}
-    </React.StrictMode>
-  );
-});
+const element = document.getElementById("root");
+const variant: Variant = element?.getAttribute("data-variant") as Variant;
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
+root.render(
+  <React.StrictMode>
+    {!variant && <div>Data variant missing</div>}
+    {variant && <App variant={variant} />}
+  </React.StrictMode>
+);
