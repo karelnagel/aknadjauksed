71b3414450de6c442fb290d09077b14dad822453
Karel <nagelkarel@gmail.com>
2023-12-05 09:42:19 +0700

    loading after dom loaded

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { type Variant } from "./store";
 
-const element = document.getElementById("root");
-const variant: Variant = element?.getAttribute("data-variant") as Variant;
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("root") as HTMLElement;
+  const variant: Variant = element?.getAttribute("data-variant") as Variant;
+  const root = ReactDOM.createRoot(element);
+  root.render(
+    <React.StrictMode>
+      {!variant && <div>Data variant missing</div>}
+      {variant && <App variant={variant} />}
+    </React.StrictMode>
+  );
+});
 
-root.render(
-  <React.StrictMode>
-    {!variant && <div>Data variant missing</div>}
-    {variant && <App variant={variant} />}
-  </React.StrictMode>
-);
